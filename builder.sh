bd9f9776cdf01acc96b2ac415317c5f184135fab
Jim Klimov <jim@jimklimov.com>
2015-10-21 14:01:56 +0200

    builder.sh : make the git part more readable

--- builder.sh
+++ builder.sh
@@ -5,13 +5,16 @@ make distclean || true
 
 # This assumes the implicit origin is dev's fork
 # and upstream is common source
-git pull --all && { git merge upstream/master || exit $?; }
+if git pull --all ; then
+	git merge upstream/master || exit $?
+fi
 
 (which gsl >/dev/null 2>&1) && \
 	gsl project.xml
 
 ./autogen.sh && \
 ./configure && \
+echo "=== MAKING PAR" && \
 if make -j -k V=0 ; then : ; else
     echo "=== REMAKING SEQ"
     make V=1
