7c7bf73b0771f004289befe8ece688d2b29a2428
Michal Vyskocil <michalvyskocil@eaton.com>
2015-10-21 14:03:15 +0200

    Problem: does not build
    Solution: add argc/argv into mains

--- src/comp.c
+++ src/comp.c
@@ -18,7 +18,7 @@ s_compute_average (zlistx_t *list) {
     return ret;
 }
 
-int main() {
+int main(int argc, char **argv) {
 
     if(argc != 2) {
         fprintf(stderr, "Usage: %s address\n", argv[0]);
